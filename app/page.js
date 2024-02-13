b98c52c55fcb79b6d215832fc749ef6f02dfe5da
Opyd <danielopyd99@gmail.com>
2024-02-13 12:41:38 +0100

    disabled ssr for App component

--- app/page.js
+++ app/page.js
@@ -1,12 +1,17 @@
 import App from "@/components/App";
 import Footer from "@/components/Footer";
+import dynamic from "next/dynamic";
+
+const Dynamic = dynamic(() => import('@/components/App'), {
+	ssr: false
+})
 
 
 export default function Home() {
 
 	return (
 		<main>
-			<App />
+			<Dynamic />
 			<Footer />
 		</main>
 	)
