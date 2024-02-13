6fea7961ee7e698a4261724a45c838d0232c1311
Opyd <danielopyd99@gmail.com>
2024-02-13 21:22:21 +0100

    mobile UI improvements

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,10 @@
 import {GitHubLogoIcon} from "@radix-ui/react-icons";
+import {Chip} from "@nextui-org/react";
 
 function Footer() {
 	return (
-		<div className="absolute right-5 bottom-3 lg:bottom-5">
-			<p className="flex items-center gap-x-2">
+		<div className="absolute top-2 right-2">
+			<p className="flex items-center gap-x-2 bg-[#3f3f46] rounded-[10px] px-2 py-1 text-xs">
 				Created with ❤️ by <GitHubLogoIcon/>
 				<a href="https://github.com/opyd" target="_blank">Opyd</a>
 			</p>
